b3b4c8a51e8ada8b08a08cdad9923f92013aabfd
Tom Kraak <tom.kraak@gmail.com>
2017-06-06 22:59:08 -0400

    add __proto__ property

--- test/objects/Function.prototype.js
+++ test/objects/Function.prototype.js
@@ -3,9 +3,11 @@ import test from 'ava'
 test('function objects should reference Function.prototype as their prototype object', t => {
   function f () {}
   t.true(Object.getPrototypeOf(f) === Function.prototype)
+  t.true(f.__proto__ === Function.prototype)
 })
 
 test('all functions have a public prototype property that references Object.prototype', t => {
   function f () {}
   t.true(Object.getPrototypeOf(f.prototype) === Object.prototype)
+  t.true(f.prototype.__proto__ === Object.prototype)
 })
--- test/objects/Object.prototype.js
+++ test/objects/Object.prototype.js
@@ -3,8 +3,10 @@ import test from 'ava'
 test('generic objects should reference Object.prototype as their prototype object', t => {
   const obj = {}
   t.true(Object.getPrototypeOf(obj) === Object.prototype)
+  t.true(obj.__proto__ === Object.prototype)
 })
 
 test('Object.prototype should be the top of the prototype chain', t => {
   t.true(Object.getPrototypeOf(Object.prototype) === null)
+  t.true(Object.prototype.__proto__ === null)
 })
