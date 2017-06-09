6ba7f2e86cf70d9559f4ae68980baee4dd357c18
Tom Kraak <thomas.kraak@teamaol.com>
2017-06-09 11:42:38 -0400

    pass undefined and null to Array.prototype.splice

--- test/arrays/Array.prototype.splice.js
+++ test/arrays/Array.prototype.splice.js
@@ -9,7 +9,27 @@ test('Array.prototype.splice should modify the array', t => {
   t.deepEqual([1, 2], t.context.numbers)
 })
 
+test('Array.prototype.splice should empty the array when invoked with undefined', t => {
+  t.context.numbers.splice(undefined)
+  t.deepEqual([], t.context.numbers)
+})
+
+test('Array.prototype.splice should empty the array when invoked with null', t => {
+  t.context.numbers.splice(null)
+  t.deepEqual([], t.context.numbers)
+})
+
 test('Array.prototype.splice should return the removed elements', t => {
-  const result = t.context.numbers.splice(2, 3)
-  t.deepEqual([3, 4, 5], result)
+  const removed = t.context.numbers.splice(2, 3)
+  t.deepEqual([3, 4, 5], removed)
+})
+
+test('Array.prototype.splice should return an unmodified array when invoked with undefined', t => {
+  const removed = t.context.numbers.splice(undefined)
+  t.deepEqual([1, 2, 3, 4, 5], removed)
+})
+
+test('Array.prototype.splice should return an unmodified array when invoked with null', t => {
+  const removed = t.context.numbers.splice(null)
+  t.deepEqual([1, 2, 3, 4, 5], removed)
 })
