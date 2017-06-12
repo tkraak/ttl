daaf1cbc8be682beefc212d58ad1ed632e7c41da
Tom Kraak <tom.kraak@gmail.com>
2017-06-12 16:32:37 -0400

    add implicit and explicit this binding

--- test/functions/this.js
+++ test/functions/this.js
@@ -1,6 +1,31 @@
 import test from 'ava'
 
 test('should be undefined in strict mode', t => {
-  function f1() { return this}
-  t.true(f1() === undefined)
+  function f () { return this }
+  t.true(f() === undefined)
 })
+
+test('should reference the implicitly bound object', t => {
+  const obj = {
+    a: 42,
+    method: function () { return this.a }
+  }
+  t.is(obj.method(), 42)
+})
+
+test('should reference the explicitly bound object via Function.prototype.call()', t => {
+  function f () { return this.a }
+  const obj = {
+    a: 42
+  }
+  t.is(f.call(obj), 42)
+})
+
+test('should reference the explicitly bound object via Function.prototype.apply()', t => {
+  function f () { return this.a }
+  const obj = {
+    a: 42
+  }
+  t.is(f.apply(obj), 42)
+})
+
