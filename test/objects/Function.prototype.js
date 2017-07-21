f1cdfef2ff82924526bd15678f4f747c7e41011b
Tom Kraak <tom.kraak@gmail.com>
2017-07-21 16:05:20 -0400

    clarify the public prototype property of functions

--- test/objects/Function.prototype.js
+++ test/objects/Function.prototype.js
@@ -6,7 +6,17 @@ test('function objects should reference Function.prototype as their prototype ob
   t.true(f.__proto__ === Function.prototype)
 })
 
-test('all functions have a public prototype property that references Object.prototype', t => {
+test('all functions have a public prototype property that references an object', t => {
+  function f () {}
+  t.true(typeof f.prototype === 'object')
+})
+
+test('the public prototype property references an object with a `constructor` property', t => {
+  function f () {}
+  t.true(f.prototype.hasOwnProperty('constructor'))
+})
+
+test('the public prototype property references an object wich in turn references Object.prototype as its prototype object', t => {
   function f () {}
   t.true(Object.getPrototypeOf(f.prototype) === Object.prototype)
   t.true(f.prototype.__proto__ === Object.prototype)
