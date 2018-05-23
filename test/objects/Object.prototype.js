ff1a87f8eae01136361c446e84461e20a9281cbb
Tom Kraak <thomas.kraak@teamaol.com>
2018-05-23 14:33:28 -0400

    add propertyless object

--- test/objects/Object.prototype.js
+++ test/objects/Object.prototype.js
@@ -10,3 +10,9 @@ test('Object.prototype should be the top of the prototype chain', t => {
   t.true(Object.getPrototypeOf(Object.prototype) === null)
   t.true(Object.prototype.__proto__ === null)
 })
+
+test('propertyless objects do not reference Object.prototype as their prototype object', t => {
+  const obj = Object.create(null)
+  t.true(Object.getPrototypeOf(obj) === null)
+  t.true(obj.__proto__ === undefined)
+})
